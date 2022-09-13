f4a37b620b842e8af01ccc3317da8d89763d7ec8
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-09-13 15:34:14 +0200

    api checked

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
+import axios from "axios";
 
 const ariaLabel = { "aria-label": "description" };
 
 const Home = () => {
+  const [title, setTitle] = useState([]);
+
+  const getMovies = async () => {
+    const res = await axios.get(
+      "https://imdb-api.com/en/API/SearchMovie/k_w7k9gevm/fast"
+    );
+    setTitle(res.data.results.splice(0, 3));
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
       <h1>HOME PAGE</h1>
       <input type={"text"} placeholder={"Search..."} className="input"></input>
+      {title.map((el) => {
+        return (
+          <div key={el.id}>
+            <h1>{el.title}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 };
